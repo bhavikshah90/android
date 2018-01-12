e70c0b25841d28e56a4fba89bee64d309383cc7b
bhavik <bhavik@github.com>
2018-01-12 17:45:19 -0500

    flappy bird clone
    Coding of famous game flappy bird.
    Trying handson on android game development using GDX engine

---
+++ Flappy Bird/core/src/com/bhavik/flappybird/FlappyBird.java
@@ -0,0 +1,72 @@
+package com.bhavik.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlappyBird extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+    Texture[] birds;
+	int flapState = 0;
+    float birdY = 0;
+    float velocity = 0;
+    int gameState = 0;
+    float gravity = 2;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+        background = new Texture("bg.png");
+        birds = new Texture[2];
+        birds[0] = new Texture("bird.png");
+        birds[1] = new Texture("bird2.png");
+        birdY = Gdx.graphics.getHeight() / 2 - birds[flapState].getHeight();
+	}
+
+	@Override
+	public void render () {
+
+
+        if(gameState != 0) {
+
+
+            if(Gdx.input.justTouched()){
+             velocity = -30;
+            }
+
+            if(birdY > 0 || velocity < 0){
+
+                velocity += gravity;
+                birdY -= velocity;
+
+            }
+
+        }
+        else{
+
+            if(Gdx.input.justTouched()){
+                Gdx.app.log("Touched","touched");
+                gameState = 1;
+
+            }
+        }
+        if (flapState == 0) {
+            flapState = 1;
+        } else {
+            flapState = 0;
+        }
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth(), birdY);
+        batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+        background.dispose();
+	}
+}
